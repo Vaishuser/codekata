18e252fd3e2edef89ccd107e2ffbfd0c48814c1f
Vaishnavi <31802995+Vaishuser@users.noreply.github.com>
2017-09-14 23:10:25 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int number;
+pritnf("Enter a number");
+scanf("%d",&num);
+if(num<=0)
+{
+if(num==0)
+printf("0");
+else
+printf("negative");
+}
+else
+printf("+ve number");
+}
