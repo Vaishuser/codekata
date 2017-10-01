91218736d172696475be8dd3b106d19bb69f2741
Vaishnavi <31802995+Vaishuser@users.noreply.github.com>
2017-10-01 16:42:44 +0530

    Add files via upload

---
+++ vowel.rb
@@ -0,0 +1,16 @@
+print "Enter your Letter: "
+grade = gets.chomp
+case grade
+when "a"
+  puts 'Vowel'
+when "e"
+  puts 'Vowel'
+when "i"
+  puts 'Vowel'
+when "o"
+  puts 'Vowel'
+when "u"
+  puts 'Vowel'
+else
+  puts " Not a  Vowel"
+end
